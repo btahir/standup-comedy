80772be9197ba5c520941feb97dface4f66eb802
btahir89 <bilal.tahir89@gmail.com>
2024-03-27 14:49:57 -0700

    add heading

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import RecordingSection from '@/components/RecordingSection'
 
 export default async function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center justify-between p-12'>
+      <h1 className='text-2xl text-center text-indigo-800 sm:text-3xl mb-6 font-bold'>
+        Standup Comedy Practice with AI
+      </h1>
       <RecordingSection />
     </main>
   )
